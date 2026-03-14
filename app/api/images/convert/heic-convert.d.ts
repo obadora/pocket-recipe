3b4d1da240b926ce375f84073ca9f9c7865f4bb1
Hiroki Kitamura <113627737+obadora@users.noreply.github.com>
2026-03-14 10:48:44 +0900

    【feature/temp】デッドコード削除 (#54)

--- app/api/images/convert/heic-convert.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'heic-convert' {
-  interface ConvertOptions {
-    buffer: ArrayBuffer
-    format: 'JPEG' | 'PNG'
-    quality?: number
-  }
-  function heicConvert(options: ConvertOptions): Promise<ArrayBuffer>
-  export default heicConvert
-}
